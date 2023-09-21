64c097bd14524ef4f215d86eadf89fa1f2f5f78d
pascal-siemer <pascal.siemer98@gmail.com>
2023-09-21 19:30:50 +0200

    Initial commit

---
+++ QuizFrontend/Data/User.cs
@@ -0,0 +1,3 @@
+namespace QuizApplication.Data;
+
+public record User(string Username, string Password);
---
+++ QuizFrontend/IncludeRequestCredentialsMessageHandler.cs
@@ -0,0 +1,16 @@
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+namespace QuizFrontend;
+
+public class IncludeRequestCredentialsMessageHandler : DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+        return base.SendAsync(request, cancellationToken);
+    }
+}
